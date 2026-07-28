39c6593c409067c027dc1f4e4f3c1c24e44bf09d
Sunflower-Alisa <150042970@qq.com>
2026-07-28 16:46:26 +0800

    Clean up git tracking: add .gitignore rules, remove WAL files, add README and DB auto-init

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,7 +1,9 @@
 const Database = require('better-sqlite3');
 const path = require('path');
+const fs = require('fs');
 
 const dbPath = path.join(__dirname, '..', 'data', 'dayloop.db');
+fs.mkdirSync(path.dirname(dbPath), { recursive: true });
 const db = new Database(dbPath);
 
 db.pragma('journal_mode = WAL');
